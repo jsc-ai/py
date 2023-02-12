f6d32bf65e67cad77ea73e1b96e890871750b36f
jsc-ai <118974351+jsc-ai@users.noreply.github.com>
2023-02-12 10:59:50 +0900

    Update sea_wind_v1.py

--- sea_wind_v1.py
+++ sea_wind_v1.py
@@ -15,7 +15,7 @@ plt.title('Correlation between Sea Temperature and Average Wave Height')
 plt.show()
 
 # 일반적으로 pandas 에서 파일 관리를 하기 때문에 close 명령을 사용할 필요는 없지만
-# 파일을 여는 개수가 많거나 큰 파일을 사용하는 경우 메모리 부족 현상이 발생할 수 있는 경우에는
+# 파일을 여러번 열어야 하는 경우나 큰 파일을 사용하는 경우 메모리 부족 현상이 발생할 수 있는 경우에는
 # close 를 해주도록 하자
 
 df.close()
